51c6d9b738ad0eeb061a4336d352e7b8c7683769
KirilGvozd <89731883+KirilGvozd@users.noreply.github.com>
2022-09-16 19:51:03 +0300

    Лабораторная работа №1 по Ассемблеру

---
+++ asm1/SE_Asm01/LP_asm1.asm
@@ -0,0 +1,32 @@
+.586P
+.MODEL FLAT, STDCALL
+includelib kernel32.lib
+
+ExitProcess PROTO : DWORD
+MessageBoxA PROTO : DWORD, : DWORD, : DWORD, : DWORD
+
+.STACK 4096
+
+.CONST
+
+.DATA
+MB_OK EQU 0
+STR1 DB "Моя первая программа", 0
+STR2 DB "Привет всем!", 0
+HW DD ?
+
+.CODE
+
+main PROC
+START :
+		PUSH MB_OK
+		PUSH OFFSET STR1
+		PUSH OFFSET STR2
+		PUSH HW
+		CALL MessageBoxA
+
+	push - 1;
+	call ExitProcess;
+main ENDP
+
+end main 
